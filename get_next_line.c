09937d945a85e9954f03567a0738fe0044fc9f36
Fbartoli <fbartoli@monaco.edu>
2018-11-27 01:57:50 +0100

    h

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fbartoli <fbartoli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 16:58:15 by fbartoli          #+#    #+#             */
-/*   Updated: 2018/11/27 01:21:44 by fbartoli         ###   ########.fr       */
+/*   Updated: 2018/11/27 01:57:33 by fbartoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,16 @@ int		get_next_line(int fd, char **line)
 	int				ret;
 	char			*buf;
 
-	buf = ft_memalloc(BUFFER_SIZE);
-	while ((ret = read(fd, buf, BUFFER_SIZE)) && !ft_strchr(buf, '\n'))
-	{
-		while (buf[ret] != '\n')
-			ret--;
-	}
+	ret = read(fd, buf, BUFFER_SIZE);
 	buf[ret] = '\0';
+	while (buf[ret] != '\n')
+		ret--;
+	printf("buf, %s:'\n'", buf);
 	*line = ft_strsub(buf, 0, ret);
 	save = ft_strsub(buf, ret, BUFFER_SIZE - ret);
 	printf("line, %s:'\n'", *line);
 	printf("save, %s:'\n'", save);
-	return (0);
+	return (1);
 	}
 
 	int	main(int argc, char ** argv)
@@ -41,7 +39,6 @@ int		get_next_line(int fd, char **line)
 	int		fd;
 	int		fd2;
 	char	*line;
-	pid_t	child;
 	char	n = '\n';
 
 	if (argc < 2)
@@ -50,34 +47,16 @@ int		get_next_line(int fd, char **line)
 		return (1);
 	}
 	fd = open(argv[1], O_RDONLY);
-	fd2 = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (fd == -1 || fd2 == -1)
 	{
 		perror("open");
 		close(fd);
-		close(fd2);
 		return (-1);
 	}
 	while (get_next_line(fd, &line) == 1)
 	{
-		write(fd2, line, strlen(line));
-		write(fd2, &n, 1);					// attention si le fichier test n a pas de \n et que ca affiche une erreur c'est normal
+		write(fd2, line, strlen(line));				// attention si le fichier test n a pas de \n et que ca affiche une erreur c'est normal
 		free(line);							// vous inquietez pas
 	}
-	close(fd);
-	close(fd2);
-	child = fork();
-	if (child == 0)
-	{
-		char	*arg[] = {"test.txt", NULL, "test.txt", NULL};
-
-		arg[1] = argv[1];
-		execve(arg[0], arg, NULL);
-		exit(0);
-	}
-	else
-		wait(NULL); // bad code I know ... but it's not the project
-	(void)argc;
-	(void)argv;
 	return (0);
 }
