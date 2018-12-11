ad5aa6169690ce183a413bb6d211491c8f4ddb96
Fbartoli <fbartoli@monaco.edu>
2018-12-11 20:41:07 +0100

    Clean up

--- main.c
+++ main.c
@@ -1,22 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbartoli <fbartoli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/01 15:57:49 by iiliuk            #+#    #+#             */
+/*   Updated: 2018/12/11 20:37:20 by fbartoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-#include <stdio.h>
 #include <fcntl.h>
-#include <string.h>
-#include <sys/wait.h>
+#include <stdio.h>
 
-int			main(int ac, char **av)
+/* main for testing 1 fd */
+int		main(int argc, char **argv)
 {
-	int		fd;
-	char	*line;
-	int		i;
+	int fd;
+	char *line;
+	int ret; /* printing gnl return value */
+	int i;
 
-	line = NULL;
-	i = 0;
-	fd = open(av[ac-1], O_RDONLY);
-	while (get_next_line(fd, &line) > 0)
+	if (argc >= 2)
 	{
-		ft_putendl(line);
-		i++;
+		if (access(argv[1], F_OK) == 0)
+		{
+			i = 0;
+			fd = open(argv[1], O_RDONLY);
+			while((ret = get_next_line(fd, &line)) == 1)
+			{
+				i++;
+				printf("MAIN:%d, line>%s\n", i, line);
+			}
+			close(fd);
+       	}
 	}
-	close(fd);
+	return (1);
 }
+
+///* main for testing multiple fds
+//int		main(int argc, char **argv)
+//{
+//	int fd;
+// 	int fd2;
+//	char *line;
+//	int ret; printing gnl return value
+//	int i;
+//
+//	if (argc >= 2)
+//	{
+//		if (access(argv[1], F_OK) == 0)
+//		{
+//			i = 0;
+//			fd = open(argv[1], O_RDONLY);
+//			while((ret = get_next_line(fd, &line)) == 1)
+//			{
+//				i++;
+//				printf("MAIN:%d, line>%s\n", ret, line);
+//			}
+//			close(fd);
+//			i = 0;
+//			fd2 = open(argv[2], O_RDONLY);
+//			while((ret = get_next_line(fd2, &line)) == 1)
+//			{
+//				i++;
+//				printf("MAIN:%d, line>%s\n", ret, line);
+//			}
+//			close(fd2);
+//		}
+//	}
+//	return (1);
+//}
+//
+// main for testing multiple fds calling gnl on first 3 lines from every file
+//int		main(int argc, char **argv)
+//{
+//	int fd;
+// 	int fd2;
+//	char *line;
+//	int ret; /* printing gnl return value
+//	int i;
+//
+//	if (argc >= 2)
+//	{
+//		if (access(argv[1], F_OK) == 0)
+//		{
+//			fd = open(argv[1], O_RDONLY);
+//			fd2 = open(argv[2], O_RDONLY);
+//			ret = get_next_line(fd, &line);
+//			if (ret == 1)
+//				printf("MAIN:%d, line1>%s\n", ret, line);
+//			ret = get_next_line(fd2, &line);
+//			if (ret == 1)
+//				printf("MAIN:%d, line1>%s\n", ret, line);
+//			ret = get_next_line(fd, &line);
+//			if (ret == 1)
+//				printf("MAIN:%d, line2>%s\n", ret, line);
+//			ret = get_next_line(fd2, &line);
+//			if (ret == 1)
+//				printf("MAIN:%d, line2>%s\n", ret, line);
+//			ret = get_next_line(fd, &line);
+//			if (ret == 1)
+//				printf("MAIN:%d, line3>%s\n", ret, line);
+//			ret = get_next_line(fd2, &line);
+//			if (ret == 1)
+//				printf("MAIN:%d, line3>%s\n", ret, line);
+//			close(fd);
+//			close(fd2);
+//		}
+//	}
+//	return (1);
+//}
+//
